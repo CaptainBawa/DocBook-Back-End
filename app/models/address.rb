621fac72556affb35430aa677d7b260f1904b5d5
Collins Bawa <collinskwamebawa@yahoo.com>
2023-08-24 17:35:03 +0000

    Add model relationship

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
+# The Address class represents an address
+# and has a many-to-many relationship with
+# the Doctor class through the DoctorAddress join table.
 class Address < ApplicationRecord
+  has_many :doctor_addresses, dependent: :destroy
+  has_many :doctors, through: :doctor_addresses
 end
