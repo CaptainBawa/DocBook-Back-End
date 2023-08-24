884cf200a98a1a2f8a53cd3155e789853d53a669
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-08-24 18:34:17 +0100

    Add devise attributes for a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@
 # with the Appointment and Doctor classes, and the
 # dependent option is set to destroy.
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable, authentication_keys: [:username]
   has_many :appointments, dependent: :destroy
   has_many :doctors, dependent: :destroy
 end
