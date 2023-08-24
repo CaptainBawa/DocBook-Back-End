aa0cb28351657631361af149dabe2ebb3e7d8600
Collins Bawa <collinskwamebawa@yahoo.com>
2023-08-24 17:35:52 +0000

    Add model relationship and comments

--- app/models/doctor_address.rb
+++ app/models/doctor_address.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
+# The DoctorAddress class is a model that
+# represents the association
+# between a doctor and an address.
 class DoctorAddress < ApplicationRecord
   belongs_to :doctor
   belongs_to :address
