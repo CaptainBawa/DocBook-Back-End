22f3935f03a2af0c539f2dfc55dd0efea3d458c7
Collins Bawa <collinskwamebawa@yahoo.com>
2023-08-24 17:36:05 +0000

    Add model relationship and comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
+# The User class has a one-to-many relationship
+# with the Appointment and Doctor classes, and the
+# dependent option is set to destroy.
 class User < ApplicationRecord
+  has_many :appointments, dependent: :destroy
+  has_many :doctors, dependent: :destroy
 end
