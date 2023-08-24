bb43b18be630f5c929397450c036dcc4d2aea240
Collins Bawa <collinskwamebawa@yahoo.com>
2023-08-24 17:35:21 +0000

    Add model relationship and comments

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
+# The Appointment class is a model that
+# represents an appointment between a user
+# and a doctor.
 class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :doctor
