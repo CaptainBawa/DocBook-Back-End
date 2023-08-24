419ce30da5a41ab59480da471c0cbc5c2360c076
Collins Bawa <collinskwamebawa@yahoo.com>
2023-08-24 17:35:34 +0000

    Add model relationship and comments

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
+# The Doctor class is a model that represents a
+# doctor and has associations with users,
+# appointments, and addresses.
 class Doctor < ApplicationRecord
   belongs_to :user
+  has_many :appointments, dependent: :destroy
+  has_many :doctor_addresses, dependent: :destroy
+  has_many :addresses, through: :doctor_addresses
 end
