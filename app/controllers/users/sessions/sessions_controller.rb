05deb2b3805e33e2122017bc3925a8071a32011f
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-08-24 18:35:26 +0100

    Add authentication controller

---
+++ app/controllers/users/sessions/sessions_controller.rb
@@ -0,0 +1,9 @@
+class Users::Sessions::SessionsController < Devise::SessionsController
+  before_action :configure_sign_in_params, only: [:create]
+
+  protected
+
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
+  end
+end
