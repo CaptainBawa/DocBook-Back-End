2228cb37ab24a5b16f4f7bb66d69a8a1996bdaea
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-08-25 11:17:27 +0100

    Modify  the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
     devise_for :users, controllers: { 
-      sessions: 'users/sessions'
+      sessions: 'users/sessions',
       registrations: 'users/registrations'
      }
 
