b4a9ab4982bd7dfbc7cef7da087c1773927c94fd
Chritelle-12 <mariechristellenirere@gmail.com>
2023-08-25 15:23:40 +0200

    Add validations and update tests for appointment model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -4,4 +4,7 @@
 class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :doctor
+
+  validates :appointment_date, presence: true
+
 end
--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -5,4 +5,7 @@ RSpec.describe Appointment, type: :model do
     it { should belong_to(:user) }
     it { should belong_to(:doctor) }
   end
+
+  it { should validate_presence_of(:appointment_date) }
+
 end
