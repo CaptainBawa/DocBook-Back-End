a8eebc3a72ff09125e9710959ee90d5fe97bfff8
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-08-25 09:01:36 +0100

    run migrations
    Update the authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users do 
-    controller :{
-      sessions: 'users/sessions',
+    devise_for :users, controllers: { 
+      sessions: 'users/sessions'
       registrations: 'users/registrations'
-    }
+     }
+
+
+ 
   resources :users do
     resources :doctors
   end
