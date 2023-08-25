cf904e6103d571ac62326f909645386207714966
Chritelle-12 <mariechristellenirere@gmail.com>
2023-08-25 15:27:20 +0200

    Updated doctor_address model validations

--- spec/models/doctor_address_spec.rb
+++ spec/models/doctor_address_spec.rb
@@ -5,4 +5,7 @@ RSpec.describe DoctorAddress, type: :model do
     it { should belong_to(:doctor) }
     it { should belong_to(:address) }
   end
+
+  it { should validate_presence_of(:doctor_id) }
+  it { should validate_presence_of(:address_id) }
 end
