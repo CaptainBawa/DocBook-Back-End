bc329a97f491ed300cf00f51141b78f25598c427
Chritelle-12 <mariechristellenirere@gmail.com>
2023-08-25 16:05:11 +0200

    Add validations and update tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,10 +9,25 @@ RSpec.describe User, type: :model do
   it 'is not valid without an email' do
     user = User.new(username: 'testuser', password: 'password')
     expect(user).to_not be_valid
+    expect(user.errors[:email]).to include("can't be blank")
   end
 
   it 'is not valid without a password' do
     user = User.new(username: 'testuser', email: 'test@example.com')
     expect(user).to_not be_valid
+    expect(user.errors[:password]).to include("can't be blank")
+  end
+
+  it 'is not valid without a username' do
+    user = User.new(email: 'test@example.com', password: 'password')
+    expect(user).to_not be_valid
+    expect(user.errors[:username]).to include("can't be blank")
+  end
+
+  it 'validates the uniqueness of username' do
+    existing_user = User.create(username: 'existinguser', email: 'existing@example.com', password: 'password')
+    user = User.new(username: 'existinguser', email: 'test@example.com', password: 'password')
+    expect(user).to_not be_valid
+    expect(user.errors[:username]).to include("has already been taken")
   end
 end
