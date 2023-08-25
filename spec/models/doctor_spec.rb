0359f6e4a92458a496044237f715a39b6796cbf9
Chritelle-12 <mariechristellenirere@gmail.com>
2023-08-25 13:35:24 +0200

    Added testing for doctor model

---
+++ spec/models/doctor_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Doctor, type: :model do
+  it "is valid with valid attributes" do
+    user = User.create(username: "testuser", email: "test@example.com", password: "password")
+    doctor = Doctor.new(user: user)
+    expect(doctor).to be_valid
+  end
+
+  it "is not valid without a user" do
+    doctor = Doctor.new
+    expect(doctor).to_not be_valid
+  end
+
+  it "has many appointments" do
+    user = User.create(username: "testuser", email: "test@example.com", password: "password")
+    doctor = Doctor.create(user: user)
+    appointment1 = doctor.appointments.create(appointment_date: Date.today)
+    appointment2 = doctor.appointments.create(appointment_date: Date.tomorrow)
+
+    expect(doctor.appointments).to include(appointment1, appointment2)
+    
+  end
+
+end
