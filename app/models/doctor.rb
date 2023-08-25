6a06d05a238a9d39fc20b4e2238026f92d09e8d3
Chritelle-12 <mariechristellenirere@gmail.com>
2023-08-25 15:59:33 +0200

    Add validations to doctor model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -6,4 +6,12 @@ class Doctor < ApplicationRecord
   has_many :appointments, dependent: :destroy
   has_many :doctor_addresses, dependent: :destroy
   has_many :addresses, through: :doctor_addresses
+
+  validates :user, presence: true
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :phone_number, presence: true
+  validates :specialty, presence: true
+  validates :price, presence: true
+
 end
