7d38a416b044b8ed3b0646f0932654f03a6d5937
Chritelle-12 <mariechristellenirere@gmail.com>
2023-08-25 16:00:38 +0200

    Add test validations for doctor model

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -3,7 +3,14 @@ require 'rails_helper'
 RSpec.describe Doctor, type: :model do
   it 'is valid with valid attributes' do
     user = User.create(username: 'testuser', email: 'test@example.com', password: 'password')
-    doctor = Doctor.new(user:)
+    doctor = Doctor.new(
+      user: user,
+      name: 'Dr. Test',
+      email: 'drtest@example.com',
+      phone_number: '1234567890',
+      specialty: 'General',
+      price: 100.0
+    )
     expect(doctor).to be_valid
   end
 
@@ -14,10 +21,26 @@ RSpec.describe Doctor, type: :model do
 
   it 'has many appointments' do
     user = User.create(username: 'testuser', email: 'test@example.com', password: 'password')
-    doctor = Doctor.create(user:)
+    doctor = Doctor.create(
+      user: user,
+      name: 'Dr. Test',
+      email: 'drtest@example.com',
+      phone_number: '1234567890',
+      specialty: 'General',
+      price: 100.0
+    )
     appointment1 = doctor.appointments.create(appointment_date: Date.today)
     appointment2 = doctor.appointments.create(appointment_date: Date.tomorrow)
 
     expect(doctor.appointments).to include(appointment1, appointment2)
   end
+
+  it { should belong_to(:user) }
+
+  it { should validate_presence_of(:user) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:phone_number) }
+  it { should validate_presence_of(:specialty) }
+  it { should validate_presence_of(:price) }
 end
