0dc67c468cb0154924101d51b0646e0e90748eac
tomdan-ai <tomudoh258@gmail.com>
2023-08-28 16:24:18 +0100

    Test Doctors API DELETE response

--- spec/controllers/doctors_controller_spec.rb
+++ spec/controllers/doctors_controller_spec.rb
@@ -26,4 +26,17 @@ RSpec.describe DoctorsController, type: :controller do
           expect(response).to have_http_status(:success)
         end
       end
+
+      describe 'DELETE destroy' do
+        it 'deletes a doctor' do
+          user = User.create(username: 'user1', email: 'user1@example.com', password: 'password')
+          doctor = Doctor.create(name: 'Dr. Smith', email: 'drsmith@example.com', phone_number: '123-456-7890', specialty: 'General Medicine', price: 300.0, user: user)
+    
+          expect {
+            delete :destroy, params: { user_id: user.id, id: doctor.id }
+          }.to change(Doctor, :count).by(-1)
+    
+          expect(response).to have_http_status(:no_content)
+        end
+      end
 end
