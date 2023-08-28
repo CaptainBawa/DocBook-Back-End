39b3f41a909404f4cb2f5480a3d38f879f28a3e4
tomdan-ai <tomudoh258@gmail.com>
2023-08-28 16:22:00 +0100

    Test API's GET respose

---
+++ spec/controllers/appointments_controller_spec.rb
@@ -0,0 +1,6 @@
+# spec/controllers/appointments_controller_spec.rb
+
+require 'rails_helper'
+
+RSpec.describe AppointmentsController, type: :controller do
+end
--- spec/controllers/doctors_controller_spec.rb
+++ spec/controllers/doctors_controller_spec.rb
@@ -3,5 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe DoctorsController, type: :controller do
-
+    describe 'GET index' do
+        it 'returns a successful response' do
+          get :index, params: { user_id: 1 }  # Provide a valid user_id
+          expect(response).to have_http_status(:success)
+        end
+      end
 end
