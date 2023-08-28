d61e406e4899a803611cf7fd6eea604aac12d52e
tomdan-ai <tomudoh258@gmail.com>
2023-08-28 16:26:05 +0100

    Write tests for appointments API GET respose with dummy data

--- spec/controllers/appointments_controller_spec.rb
+++ spec/controllers/appointments_controller_spec.rb
@@ -10,4 +10,22 @@ RSpec.describe AppointmentsController, type: :controller do
         end
       end
     
+      describe 'GET show' do
+        it 'returns a successful response' do
+          user = User.create(username: 'user1', email: 'user1@example.com', password: 'password')
+          doctor = Doctor.create(
+            name: 'Dr. Smith',
+            email: 'drsmith@example.com',
+            phone_number: '123-456-7890',
+            specialty: 'General Medicine',
+            price: 300.0,
+            user: user
+          )
+          appointment = Appointment.create(appointment_date: Date.today, user: user, doctor: doctor)
+    
+          get :show, params: { id: appointment.id }
+          expect(response).to have_http_status(:success)
+        end
+      end
+    
 end
