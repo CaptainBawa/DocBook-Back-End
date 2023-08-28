f900fb33a334f3f255242a711cc3f951e7f80c31
tomdan-ai <tomudoh258@gmail.com>
2023-08-28 16:25:25 +0100

    Write tests for appointments API GET respose

--- spec/controllers/appointments_controller_spec.rb
+++ spec/controllers/appointments_controller_spec.rb
@@ -3,4 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe AppointmentsController, type: :controller do
+    describe 'GET index' do
+        it 'returns a successful response' do
+          get :index
+          expect(response).to have_http_status(:success)
+        end
+      end
+    
 end
