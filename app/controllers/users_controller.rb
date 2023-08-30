7148ce15c70d4676d1c1af8185e9ce8a0f33cbfc
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-08-30 14:07:04 +0100

    Add controller for the users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+
+    if @user.save
+      redirect_to @user, notice: 'User successfully created!'
+    else
+      render action: 'new'
+    end
+  end
+end
